2d4a882c17fe2e28a38fcedb8f189eca5f2783d0
Colin Woodbury <colin@fosskers.ca>
2021-08-24 17:18:22 -0700

    [doom] `colin/seed-watch-scss`
    Much improved from the old version.

--- .doom.d/autoload.el
+++ .doom.d/autoload.el
@@ -190,10 +190,11 @@ Returns nil otherwise."
 
 ;;;###autoload
 (defun colin/seed-watch ()
-  "Alongside a top level `cargo-watch', open a `cargo-make-watch' for every sub library that has a `Makefile.toml'."
+  "Open a `cargo-make-watch' for every sub library that has a `Makefile.toml'.
+Also opens a top level 'cargo watch' and 'sass --watch' for every `.scss' file it can find."
   (interactive)
   (when-let* ((project-root (doom-project-root))
-              (make-files (directory-files-recursively project-root "Makefile.toml"))
+              (make-files (directory-files-recursively project-root "^Makefile.toml$"))
               (dirs (mapcar (lambda (file) (file-name-directory file)) make-files))
               (first (car dirs))
               (buffer (current-buffer)))
@@ -202,7 +203,16 @@ Returns nil otherwise."
     (tear-off-window nil)
     (dolist (dir (cdr dirs))
       (colin/in-terminal (format "cd %s; cargo make watch" dir)))
+    (colin/seed-watch-scss project-root)
     (switch-to-buffer-other-frame buffer)))
+
+;;;###autoload
+(defun colin/seed-watch-scss (root)
+  "Open a 'sass --watch' session for every `.scss' found from the given ROOT downwards."
+  (let ((files (directory-files-recursively root "[.]scss$")))
+    (dolist (scss files)
+      (let ((css (file-name-with-extension scss "css")))
+        (colin/in-terminal (format "sass --watch %s %s" scss css))))))
 
 ;;;###autoload
 (defun colin/cargo-watch ()
